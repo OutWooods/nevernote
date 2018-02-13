65e2c13f4cc1b1594773a3ec37fba56a984c6af2
Tagrand <thomas.a.grand@gmail.com>
2018-02-13 16:44:46 +0000

    check createNote stores a new note

--- src/models/noteList.js
+++ src/models/noteList.js
@@ -1,10 +1,24 @@
 (function(exports){
-  function NoteList(note){
-    this.notes = []
+
+  function NoteList(noteObject){
+    this.store = []
+    this.noteObject = noteObject;
   }
 
-  NoteList.prototype.addNote =  function(note){
-    this.notes.push(note)
+  NoteList.prototype.addNewNote =  function(content){
+    console.log('1')
+    let newNote = this.createNote(content);
+    console.log('2')
+    this.store.push(newNote);
   }
+
+  NoteList.prototype.createNote = function(content) {
+     return this.noteObject.createNote(content);
+  }
+
+  NoteList.prototype.notes = function() {
+    return this.store.slice()
+  }
+
   exports.NoteList = NoteList;
 })(this);
--- test/noteListTest.js
+++ test/noteListTest.js
@@ -1,7 +1,10 @@
+var newNote = {}
+var note = { createNote: function() {return newNote} };
+
 function testStoresNewNote(){
-  var noteList = new NoteList()
-  noteList.addNote("Hello")
-  assert.isTrue(noteList.notes.length === 1, 'Notelist length increases by 1')
+  var noteList = new NoteList(note);
+  noteList.addNewNote("Hello");
+  assert.isTrue(noteList.notes()[0] === newNote, 'Notelist stores a new note');
 }
 
-testStoresNewNote()
+testStoresNewNote();
