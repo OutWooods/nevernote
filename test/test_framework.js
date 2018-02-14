e150471c2c12cc0a53096f7ed71b48f64996194f
Tagrand <thomas.a.grand@gmail.com>
2018-02-14 16:52:41 +0000

    add feature tests to test file

--- test/test_framework.js
+++ test/test_framework.js
@@ -15,3 +15,19 @@ var assert = {
    }
   }
 }
+
+function fillInForm(id, string) {
+    document.getElementById(id).value = string
+  }
+
+  function clickButton(id) {
+    document.getElementById(id).click();
+  }
+
+  function hasContent(id, string) {
+    return document.getElementById(id).innerHTML.includes(string);
+  }
+
+  function doesntHaveContent(id, string) {
+   return !(document.getElementById(id).innerHTML.includes(string));
+  }
