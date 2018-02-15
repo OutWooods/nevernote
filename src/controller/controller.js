331b002816cbd276c37c3b5ce7896efce444de65
Alexy <alexandra.fina@yahoo.com>
2018-02-15 15:14:58 +0000

    Hashchange href

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -15,9 +15,31 @@ function submitButtonControl(){
   })
 }
 
+function showSelectedNote() {
+  console.log("hamster")
+  console.log(window)
+  window.addEventListener("hashchange", findNote);
+}
+
+function findNote (){
+  console.log("good")
+  displayNote(findIndex(window.location))
+}
+
+function displayNote (index){
+  document.getElementById('notebody').innerHTML = noteList().notes()[index].text
+}
+
+function findIndex(location){
+  console.log("hello")
+  console.log(location);
+  return location.hash.split("#")[1];
+}
+
 function onLoad() {
   NoteListInstance = new NoteList(Note)
   submitButtonControl();
+  showSelectedNote();
   // currently including running tests here
   runTests();
 }
--- src/views/view.js
+++ src/views/view.js
@@ -5,6 +5,7 @@
   }
   function addNoteLink() {
     var link = document.createElement('a');
+    link.href = "#" + (noteList().notes().length - 1);
     link.innerHTML = noteList().latestNote().abbreviation;
     link.id = noteList().latestNote().abbreviation;
     document.getElementById('notelist').appendChild(link);
