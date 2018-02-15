eda3cc8081d82616f169e8026501bb5312f10ac5
Alexy <alexandra.fina@yahoo.com>
2018-02-15 12:19:26 +0000

    test click Note link (no working click Link function)

---
+++ test/feature_tests/clickNoteLinkTest.js
@@ -0,0 +1,9 @@
+function visitANote(){
+  fillInForm('noteContent', "This is the best note taking site ever!");
+  clickButton('saveNote');
+  fillInForm('noteContent', "Hello you there, gorgeous person in the world!");
+  clickButton('saveNote');
+  clickLink();
+  assert.isTrue(hasContent('notebody', 'This is the best note taking site ever!'), 'notebody should display whole text')
+  assert.isTrue(doesntHaveContent('notebody', 'Hello you there'), 'notebody should not display: Hello you there')
+}
