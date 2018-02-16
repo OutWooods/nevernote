4c95b6636165f9c2d389f740b5b59dd9ab0bf9be
Tagrand <thomas.a.grand@gmail.com>
2018-02-16 13:07:46 +0000

    asserts throw errors if they fail

--- test/test_framework.js
+++ test/test_framework.js
@@ -1,11 +1,11 @@
-function addElement (message) {
+function addSuccessDiv (message) {
   newDiv = createDiv("success", message);
   var currentDiv = document.getElementById('div1');
   currentDiv.appendChild(newDiv);
 }
 
-function addPrettifiedError(err) {
-  messageDiv = createDiv("errorMessage", 'Failure: ' + err.message);
+function addErrorStack(err) {
+  messageDiv = createDiv("fail", 'Failure: ' + err.message);
   stackDiv = createDiv("stack", err.stack);
   locationDiv = createDiv("location", err.fileName + ' at ' + err.lineNumber + ':' + err.columnNumber );
 
@@ -18,43 +18,6 @@ function addPrettifiedError(err) {
   currentDiv.appendChild(failureDiv);
 }
 
-
-
-var it = function(message, tester) {
-       var result = tester();
-       var id = (result.includes('success')) ? 'success' : 'fail'
-       addElement(`${message}: ${result}`, id);
-}
-
-// var throw = function(){
-//      try{
-//         throw new Error(message);
-//       }
-//       catch(err) {
-//         addPrettifiedError(err);
-//       }
-//     }
-// }
-
-var assert = {
-  isTrue: (value, message) => {
-    if(!value) {
-      return `Failure: It should be true but is false...`;
-    } else {
-      return `Success! It should be true, and it is true...`;
-   }
-},
-
-  equals: (actual, expected) => {
-    if(actual === expected) {
-      return `Success! Actual: ${actual}, expected: ${expected}`
-    }
-    else {
-     return `Failure!!!! Actual: ${actual}, expected: ${expected}`
-    }
-  }
-}
-
 function createDiv(id, text) {
   var Div = document.createElement("div");
   Div.id = id;
@@ -63,8 +26,34 @@ function createDiv(id, text) {
   return Div;
 }
 
+var it = function(message, tester) {
+       try{
+         tester()
+       }
+       catch(err) {
+         console.error(err)
+          return addErrorStack(err)
+       }
+      return  addSuccessDiv(message)
+}
 
 
+var assert = {
+  isTrue: (value, message) => {
+    if(!value) { throw `Failure: It should be true but is false...`;  }
+},
+
+
+  equals: (actual, expected) => {
+    if(actual != expected) {
+     throw `Failure!!!! Actual: ${actual}, expected: ${expected}`
+    }
+  }
+}
+
+
+
+//feature tests
 function fillInForm(id, string) {
     document.getElementById(id).value = string
   }
