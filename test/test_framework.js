abb6c3e17cbb0a3c7cbc064d23e735df623ba6c1
philb56 <phil.bugden@outlook.com>
2018-02-16 12:15:53 +0000

    Added addPrettifiedError - note: location not showing as yet - needs fixing

--- test/test_framework.js
+++ test/test_framework.js
@@ -1,7 +1,5 @@
 function addElement (message) {
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode(message);
-  newDiv.appendChild(newContent);
+  newDiv = createDiv("success", message);
   var currentDiv = document.getElementById('div1');
   currentDiv.appendChild(newDiv);
 }
@@ -13,15 +11,35 @@ var assert = {
         throw new Error(message);
       }
       catch(err) {
-        addElement(err.stack);
+        addPrettifiedError(err);
       }
-
-      // addElement(`Failure: ${message}`);
     }
     else {
       addElement(`Success: ${message}`);
    }
   }
+}
+
+function createDiv(id, text) {
+  var Div = document.createElement("div");
+  Div.id = id;
+  var message = document.createTextNode(text);
+  Div.appendChild(message);
+  return Div;
+}
+
+function addPrettifiedError(err) {
+  messageDiv = createDiv("errorMessage", 'Failure: ' + err.message);
+  stackDiv = createDiv("stack", err.stack);
+  locationDiv = createDiv("location", err.fileName + ' at ' + err.lineNumber + ':' + err.columnNumber );
+
+  failureDiv = createDiv("failure", "");
+  failureDiv.appendChild(messageDiv);
+  failureDiv.appendChild(stackDiv);
+  failureDiv.appendChild(locationDiv);
+
+  var currentDiv = document.getElementById('div1');
+  currentDiv.appendChild(failureDiv);
 }
 
 function fillInForm(id, string) {
