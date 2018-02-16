9dedc3637c79a3b42301e12ef233e11148c367d7
Tagrand <thomas.a.grand@gmail.com>
2018-02-16 12:01:45 +0000

    add it statement to test framework

--- test/test_framework.js
+++ test/test_framework.js
@@ -1,18 +1,39 @@
-function addElement (message) {
+function addElement (message, id) {
   var newDiv = document.createElement("div");
+  newDiv.id = id
   var newContent = document.createTextNode(message);
   newDiv.appendChild(newContent);
   var currentDiv = document.getElementById('div1');
   currentDiv.appendChild(newDiv);
 }
 
+var it = function(message, tester) {
+       var result = tester();
+       var id = (result.includes('Success')) ? 'success' : 'fail'
+       console.log(result)
+       console.log(result.includes('success'))
+       addElement(`${message}: ${result}`, id);
+}
+
+
+
+
 var assert = {
-  isTrue: function(value, message) {
+  isTrue: (value, message) => {
     if(!value) {
       addElement(`Failure: ${message}`);}
     else {
       addElement(`Success: ${message}`);
    }
+},
+
+  equals: (actual, expected) => {
+    if(actual === expected) {
+      return `Success! Actual: ${actual}, expected: ${expected}`
+    }
+    else {
+     return `Failure!!!! Actual: ${actual}, expected: ${expected}`
+    }
   }
 }
 
